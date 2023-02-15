6357675b6be61bbe29de01e18c68c76df44a2f63
daphnekimyh <101921685+daphnekimyh@users.noreply.github.com>
2023-02-15 22:19:25 +0900

    Add files via upload

---
+++ app-if.js
@@ -0,0 +1,34 @@
+
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!")
+// } else {
+//     console.log("YOUR NUMBER IS GREATER(OR EQUAL) THAN 0.5!!!")
+// }
+// console.log(random);
+
+
+// const dayOfWeek = prompt('ENTER A DAY').toLowerCase();
+
+// if (dayOfWeek === 'monday') {
+//     console.log("UGHHH I HATE MONDAYS!");
+// } else if (dayOfWeek === 'saturday') {
+//     console.log("YAY I LOVE SATURDAY!");
+// } else if (dayOfWeek === 'friday') {
+//     console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+// } else {
+//     console.log("MEH");
+// }
+
+
+const age = 89;
+
+if (age < 5) {
+    console.log("You are a baby. You get in for a free");
+} else if (age < 10) {
+    console.log("You are a child. You pay $10");
+} else if (age < 65) {
+    console.log("You are an adult. You pay $20");
+} else {
+    console.log("You are a senior. You pay $10");
+}
