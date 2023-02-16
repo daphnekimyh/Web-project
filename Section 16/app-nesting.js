e9df49968dcddcd7859a86d0d6708efb3ab0f7e5
daphnekimyh <101921685+daphnekimyh@users.noreply.github.com>
2023-02-16 19:25:54 +0900

    Add files via upload

---
+++ Section 16/app-nesting.js
@@ -0,0 +1,12 @@
+const password = prompt("please enter a new password");
+
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+        console.log("Valid Password!")
+    } else {
+        console.log("Password cannot contain spaces!")
+    }
+} else {
+    console.log("PASSWORD TOO SHORT! Must be 6+ characters")
+}
+
