af536474e55775f8d8b53a6932b69ef6d4241d98
daphnekimyh <101921685+daphnekimyh@users.noreply.github.com>
2023-02-17 19:51:27 +0900

    Add files via upload

---
+++ Section 19/app-while.js
@@ -0,0 +1,59 @@
+// let count = 0;
+// while (count < 10) {
+//     console.log(count);
+//     count++;
+// }
+
+// const SECRET = "BabyHippo";
+
+// let guess = prompt("eneter the secret code...");
+// while (guess !== SECRET) {
+//     guess = prompt("enter the secret code...")
+// }
+// console.log("CONGRATS YOU GOT THE SECRET!!!")
+
+// let input = prompt("Hey, say something!")
+
+// while (true) {
+//     input = prompt(input);
+//     if (input.toLowerCase() === "stop copying me") {
+//         break;
+//     }
+// }
+// console.log("OK YOU WIN!")
+
+// for (let i = 0; i < 1000; i++) {
+//     console.log(i);
+//     if (i === 100) break;
+// }
+
+
+let maximum = parseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+    maximum = parseInt(prompt("Enter the a valid number!"));
+}
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt("Too high! Enter a new guess:");
+    } else {
+        guess = prompt("Too low! Enter a new guess:");
+    }
+}
+if (guess === 'q') {
+    console.log("OK, YOU QUIT!");
+} else {
+    console.log("CONGRATS! YOU WIN!")
+    console.log(`YOU GOT IT It took you ${attempts} guesses`);
+
+}
+
+
