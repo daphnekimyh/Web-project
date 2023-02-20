53e91650d83b3da245a0aae54395f37cfc234f0a
daphnekimyh <101921685+daphnekimyh@users.noreply.github.com>
2023-02-20 20:10:33 +0900

    Add files via upload

---
+++ Section 21/app-scope.js
@@ -0,0 +1,44 @@
+// totalEggs = 6;
+// function collectEggs() {
+//     totalEggs = 6;
+
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
+
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     let bird = 'Great Blue Heron';
+// }
+// birdWatch();
+
+// let radius = 8;
+// if (radius > 0) {
+//     const PI = 3.14159;
+//     let msg = 'HIII!'
+// }
+
+// console.log(radius);
+// console.log(PI);
+
+
+// for (let i = 0; i < 5; i++) {
+//     let msg = "ASKLDJAKLSJD";
+//     console.log(msg)
+// }
+// console.log(msg)
+
+function bankRobbery() {
+    const heroes = ['Spiderman', 'Wolverine', 'Black Panther', 'Batwoman']
+    function cryForHelp() {
+        function inner() {
+            for (let hero of heroes) {
+                console.log(`PLEASE HELP US, ${hero.toUpperCase()}`);
+            }
+        }
+        inner();
+    }
+    cryForHelp();
+}
+bankRobbery();
