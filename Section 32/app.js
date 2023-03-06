92e9a77e9b40239db143be9ce75fa957df32418a
daphnekimyh <101921685+daphnekimyh@users.noreply.github.com>
2023-03-06 22:40:42 +0900

    Add files via upload

---
+++ Section 32/app.js
@@ -0,0 +1,7 @@
+const math = require('./math');
+
+const cats = require('./shelter');
+// console.log(math.PI);
+
+// console.log(math.square(9));
+console.log("REQUIRE AN INTIRE DIRECTORY:", cats);
---
+++ Section 32/math.js
@@ -0,0 +1,21 @@
+module.exports.add = (x, y) => x + y;
+
+const PI = 3.141592;
+
+const square = x => x * x;
+
+exports.square = square;
+exports.PI = PI;
+
+
+// const math = {
+//     add: add,
+//     PI: PI,
+//     square: square
+// }
+
+// module.exports.math = math;
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
